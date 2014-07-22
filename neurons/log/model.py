5d17b934d89ead173ced8dccccb17531d42b35fb
Burak Arslan <burak.arslan@arskom.com.tr>
2014-07-22 18:28:31 +0300

    logging subsystem initial revision

---
+++ neurons/log/model.py
@@ -0,0 +1,60 @@
+# encoding: utf8
+#
+# This file is part of the Neurons project.
+# Copyright (c), Burak Arslan <burak.arslan@arskom.com.tr>,
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the {organization} nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+from neurons import TableModel
+
+from spyne import Integer64, DateTime, Unicode, AnyXml, AnyDict, Integer
+from spyne.util import memoize
+
+
+class LogEntryMixin(TableModel):
+    __mixin__ = True
+    __table_args__ = {"sqlite_autoincrement": True}
+
+    id = Integer64(primary_key=True)
+    time = DateTime(timezone=False)
+    user = Unicode(256, index='btree')
+    method = Unicode(64, index='btree')
+    req_xml = AnyXml
+    req_json = AnyDict(store_as='json')
+    err_code = Integer
+    resp_err = AnyDict(store_as='json')
+    resp_int = Integer
+    duration = Integer
+
+
+@memoize
+def TLogEntry():
+    class LogEntry(LogEntryMixin):
+        __namespace__ = 'neurons.base'
+        __tablename__ = 'neurons_log'
+    return LogEntry
