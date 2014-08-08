422e7da577cdf015ddb577a5446e8a793bc4595a
Burak Arslan <burak.arslan@arskom.com.tr>
2014-08-08 05:03:24 +0300

    import all protocols to neurons.protocol

--- neurons/protocol/__init__.py
+++ neurons/protocol/__init__.py
@@ -29,3 +29,6 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
+
+from neurons.protocol.form import HtmlForm
+from neurons.protocol.form_table import HtmlFormTable
