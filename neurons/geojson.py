60462c0c6a8aafe45f85050e5977788e1b2800ae
Burak Arslan <burak.arslan@arskom.com.tr>
2014-09-15 23:37:42 +0300

    add docs to neurons.geojson

--- neurons/geojson.py
+++ neurons/geojson.py
@@ -1,3 +1,52 @@
+# encoding: utf8
+#
+# This file is part of the Neurons project.
+# Copyright (c), Arskom Ltd. (arskom.com.tr),
+#                Burak Arslan <burak.arslan@arskom.com.tr>.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the Arskom Ltd. nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+"""
+Implementation of the GeoJson in terms of Spyne types.
+
+See http://geojson.org/ for more info.
+
+You need to use ``JsonDocument(polymorphic=True)`` for this to work.
+
+>>> from spyne.util.dictdoc import get_object_as_json
+>>> from neurons,geojson import Feature, Point, Position
+>>> f = Feature(
+...     geometry=Point(Position(1,2)),
+...     properties=dict(key="value"),
+... )
+>>> get_object_as_json(f, Feature, complex_as=dict, polymorphic=True)
+'{"type": "Feature", "geometry": {"type": "Point", "coordinates": [1, 2]}, "properties": {"key": "value"}}'
+"""
 
 from spyne import Unicode, ComplexModel, Double, Array, AnyDict
 
@@ -70,7 +119,7 @@ class Feature(ComplexModel):
     _type_info = [
         ('type', Unicode(default="Feature")),
         ('geometry', Geometry),
-        ('properies', AnyDict),
+        ('properties', AnyDict),
     ]
 
 class FeatureCollection(ComplexModel):
