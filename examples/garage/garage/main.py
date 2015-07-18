dd2b80c83f72c25592a5ef1cbd06fa4692d48444
Burak Arslan <burak.arslan@arskom.com.tr>
2015-07-18 03:43:37 +0300

    add a sample garage project, initial revision

---
+++ examples/garage/garage/main.py
@@ -0,0 +1,52 @@
+# encoding: utf8
+#
+# This file is part of the Neurons project.
+# Copyright (c), Arskom Ltd. (arskom.com.tr),
+#                Burak Arslan <burak.arslan@arskom.com.tr>.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the Arskom Ltd. nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+# NO IMPORTS IN MAIN FUNCTION!!!
+
+def init(config):
+    """Daemon initialization function. This is run after daemon configuration is
+    applied. In other words, facilities like logging, database connections etc.
+    all work at this point.
+    """
+
+    from garage.application import start_garage
+
+    return [
+        ('garage', start_garage)
+    ]
+
+
+def main_daemon():
+    import sys
+    from neurons.daemon.main import main
+    return main('garage', sys.argv, init)
---
+++ examples/garage/setup.py
@@ -0,0 +1,37 @@
+#!/bin/env python
+# encoding: utf-8
+
+import os
+import re
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+ROOT_NAME = 'garage'
+
+v = open(os.path.join(os.path.dirname(__file__), ROOT_NAME, '__init__.py'))
+VERSION = re.compile(r".*__version__ = '(.*?)'", re.S).match(v.read()).group(1)
+
+
+setup(
+    name=ROOT_NAME,
+    version=VERSION,
+    packages=find_packages(),
+
+    install_requires=['neurons>=0.2'],
+    entry_points={
+        'console_scripts': [
+            'garage_daemon=garage.main:main_daemon',
+        ],
+    },
+
+    package_data={
+
+    },
+
+    author='Burak Arslan',
+    author_email='burak.arslan@arskom.com.tr',
+
+    description="Example Garage Management Project",
+)
