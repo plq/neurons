d62be6ce5d2363848b521798a0bfd7452ac3b523
Burak Arslan <burak.arslan@arskom.com.tr>
2018-10-17 16:34:31 +0300

    implement label generation shortcut func _write_elt_with_label

--- neurons/form/widget.py
+++ neurons/form/widget.py
@@ -207,6 +207,13 @@ class HtmlFormWidget(HtmlBase):
 
         return retval
 
+    def _write_elt_with_label(self, ctx, cls, parent, name, elt):
+        label = self._gen_label_for(ctx, cls, name)
+        attrib = self._gen_label_wrapper_class(ctx, cls, name)
+        with parent.element('div', attrib=attrib):
+            parent.write(label)
+            parent.write(elt)
+
     def _gen_input_elt_id(self, name, array_index=None, **kwargs):
         if array_index is None:
             return "%s_input" % (self.selsafe(name),)
